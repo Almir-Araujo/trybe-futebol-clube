e6d2ec529a78acefbf92445f773a4069d7263192
Almir-Araujo <araujo.almir@gmail.com>
2022-10-28 14:11:06 -0300

    Requisito 09 - OK

--- app/backend/src/services/UserService.ts
+++ app/backend/src/services/UserService.ts
@@ -1,6 +1,7 @@
 import createToken from '../helpers/TokenManager/TokenManager';
 import UserModel from '../database/models/UsersModel';
 import UnauthorizedError from '../errors/UnauthorizedError';
+// import compare from 'src/helpers/TokenManager/Compare';
 
 interface Irequest {
   email: string;
@@ -9,7 +10,13 @@ interface Irequest {
 
 export default class UserService {
   login = async ({ email, password }: Irequest): Promise<string> => {
-    const user = await UserModel.findOne({ where: { email, password } });
+    const emailRegex = /^([a-z\d.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+
+    if (!emailRegex.test(email)) throw new UnauthorizedError('Incorrect email or password');
+
+    const user = await UserModel.findOne({ where: { email } });
+
+    console.log(user?.getDataValue);
 
     if (!user) throw new UnauthorizedError('Incorrect email or password');
 
