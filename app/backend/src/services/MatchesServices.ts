a1ef60b9e9aa4c9f921fded7b1066e97afaecffd
Almir-Araujo <araujo.almir@gmail.com>
2022-11-03 23:02:17 -0300

    Update

--- app/backend/src/services/MatchesServices.ts
+++ app/backend/src/services/MatchesServices.ts
@@ -1,3 +1,5 @@
+import NotFoundError from '../errors/NotFoundError';
+import EqualTeamsError from '../errors/EqualTeamsError';
 import IMatch from '../database/models/entities/IMatch';
 import MatchesModel from '../database/models/MatchesModel';
 import Team from '../database/models/TeamsModel';
@@ -41,6 +43,15 @@ export default class TeamsService {
   };
 
   insertNewMatch = async (match: IMatch) => {
+    if (match.awayTeam === match.homeTeam) {
+      throw new EqualTeamsError('It is not possible to create a match with two equal teams');
+    }
+
+    const awayTeam = await MatchesModel.findByPk(Number(match.awayTeam));
+    const homeTeam = await MatchesModel.findByPk(Number(match.homeTeam));
+
+    if (!awayTeam || !homeTeam) throw new NotFoundError('There is no team with such id!');
+
     const { id } = await MatchesModel.create(match);
     const response = await MatchesModel.findByPk(id);
     return response;
